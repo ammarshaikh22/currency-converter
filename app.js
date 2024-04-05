3cc554471e763983b94d1690b34faffd59653ad1
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2024-04-06 01:57:20 +0500

    e

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+//make the object of based currency
 let currency = {
     PKR: 1, // base currency
     USD: 0.003597,
@@ -7,16 +8,17 @@ let currency = {
     IND: 0.299621,
     BTP: 0.002847
 };
+//ask the ques to user
 const askToUser = await inquirer.prompt([
     { message: "Enter currency from", name: "from", type: "list", choices: ["PKR", "USD", "EUR", "IND", "BTP"] },
     { message: "Enter currency to", name: "to", type: "list", choices: ["PKR", "USD", "EUR", "IND", "BTP"] },
     { message: "Enter your amount", name: "amount", type: "number" }
 ]);
-let fromUser = askToUser.from;
-let toUser = askToUser.to;
-let userCurrencyFrom = currency[fromUser];
-let userCurrencyTo = currency[toUser];
-let userAmount = askToUser.amount;
-let convertInBase = userAmount / userCurrencyFrom;
-let nowConvertTo = Math.round(convertInBase * userCurrencyTo).toFixed(2);
-console.log(`The amount from ${fromUser} to ${toUser} is ${nowConvertTo}`);
+let fromUser = askToUser.from; //get currency from user converted from
+let toUser = askToUser.to; //get currency from user converted to
+let userCurrencyFrom = currency[fromUser]; //according to user from currency get base value dynamic pattern 
+let userCurrencyTo = currency[toUser]; //according to user to currency get base value dynamic pattern 
+let userAmount = askToUser.amount; //get amount from user 
+let convertInBase = userAmount / userCurrencyFrom; // divide the amount of user in our base currency
+let nowConvertTo = Math.round(convertInBase * userCurrencyTo).toFixed(2); //then multiply the the total of base currency to user currency and fixed the value of number to 2 then round of the number 
+console.log(`The amount from ${fromUser} to ${toUser} is ${nowConvertTo}`); //in last show the value in console
--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 
+//make the object of based currency
 let currency:any= {
     PKR: 1,// base currency
     USD: 0.003597,
@@ -9,6 +10,7 @@ let currency:any= {
     BTP: 0.002847
 }
 
+//ask the ques to user
 const askToUser = await inquirer.prompt([
     { message: "Enter currency from", name: "from", type: "list", choices: ["PKR", "USD", "EUR", "IND", "BTP"] },
     { message: "Enter currency to", name: "to", type: "list", choices: ["PKR", "USD", "EUR", "IND", "BTP"] },
@@ -16,12 +18,12 @@ const askToUser = await inquirer.prompt([
 ])
 
 
-let fromUser = askToUser.from
-let toUser = askToUser.to
-let userCurrencyFrom = currency[fromUser]
-let userCurrencyTo = currency[toUser]
-let userAmount = askToUser.amount
+let fromUser = askToUser.from //get currency from user converted from
+let toUser = askToUser.to //get currency from user converted to
+let userCurrencyFrom = currency[fromUser] //according to user from currency get base value dynamic pattern 
+let userCurrencyTo = currency[toUser] //according to user to currency get base value dynamic pattern 
+let userAmount = askToUser.amount //get amount from user 
 
-let convertInBase = userAmount / userCurrencyFrom
-let nowConvertTo = Math.round(convertInBase * userCurrencyTo).toFixed(2)
-console.log(`The amount from ${fromUser} to ${toUser} is ${nowConvertTo}`)
+let convertInBase = userAmount / userCurrencyFrom // divide the amount of user in our base currency
+let nowConvertTo = Math.round(convertInBase * userCurrencyTo).toFixed(2) //then multiply the the total of base currency to user currency and fixed the value of number to 2 then round of the number 
+console.log(`The amount from ${fromUser} to ${toUser} is ${nowConvertTo}`) //in last show the value in console

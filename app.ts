cf5734a776b9332527d1108a3956a7503ebb51e2
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2024-04-06 01:44:26 +0500

    Initial commit

---
+++ app.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let currency:any= {
+    PKR: 1,// base currency
+    USD: 0.003597,
+    EUR: 0.003320,
+    IND: 0.299621,
+    BTP: 0.002847
+}
+
+const askToUser = await inquirer.prompt([
+    { message: "Enter currency from", name: "from", type: "list", choices: ["PKR", "USD", "EUR", "IND", "BTP"] },
+    { message: "Enter currency to", name: "to", type: "list", choices: ["PKR", "USD", "EUR", "IND", "BTP"] },
+    { message: "Enter your amount", name: "amount", type: "number" }
+])
+
+
+let fromUser = askToUser.from
+let toUser = askToUser.to
+let userCurrencyFrom = currency[fromUser]
+let userCurrencyTo = currency[toUser]
+let userAmount = askToUser.amount
+
+let convertInBase = userAmount / userCurrencyFrom
+let nowConvertTo = Math.round(convertInBase * userCurrencyTo).toFixed(2)
+console.log(`The amount from ${fromUser} to ${toUser} is ${nowConvertTo}`)
